4cd370e74ac034c586876d1f852eedb5d5676dd5
jx1111-dev <buscaalexcatalin@gmail.com>
2025-11-14 11:57:54 +0000

    stil unifished

--- assessed_exercise_three.py
+++ assessed_exercise_three.py
@@ -1,19 +1,34 @@
 winning_list = [5, 17, 14]
-guessed_list = [5, 14, 17]
+guessed_list = [6, 14, 4]
 
 
 def winning_numbers(winning_list, guessed_list):
 
-    if guessed_list[0] in guessed_list:
+    if guessed_list[0] in winning_list:
+        num1 = True
+    else:
+        num1 = False
+    
+    if guessed_list[1] in winning_list:
+        num2 = True
+    else:
+        num2 = False
 
-        if guessed_list[1] in guessed_list:
+    if guessed_list[2] in winning_list:
+        num3 = True
+    else:
+        num3 = False
 
-            if guessed_list[2] in guessed_list:
+    if (num1 and num2 and num3) == True:
+        print("First")
 
-                return "First"
+    elif (num1 and num2) or (num2 and num3) or (num1 or num3) == True:
+        print("Second")
 
-        else:
-
-            return "Second"
+    elif num1 or num2 or num3 == True:
+        print("Third")
 
     else:
+        print("No")
+
+winning_numbers(guessed_list, winning_list)
