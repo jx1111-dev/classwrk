70098c120dcc14d2f7eaf967135cd782c0b77ce9
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-03 17:19:42 +0000

    added comments to exericses 1 and 2

--- ass_ex_one.py
+++ ass_ex_one.py
@@ -1,20 +1,25 @@
 def calculator():
     
+    #This contains the list of operators the calculator can use
     operator_list = ["+","-","*","/","<","<=",">",">="]
 
+    #try statement is used for validation
     try:
         num1 = int(input("Enter the first number: "))
         num2 = int(input("Enter the second number: "))
         operator = input("Enter the operator: ")
 
+        #the program ends if the operator isnt in the list
         if operator not in operator_list:
             return print("Invalid operator")
     
+    #executes in the case of an error
     except:
         print("Your inputs are invalid. Try again.")
     
     else:
 
+        #performs calculations with all the operators
         if operator =="+":
             result = num1 + num2
 
@@ -47,5 +52,3 @@ def calculator():
             result = num1>=num2
 
         return print(f"Your result is: {result}")
-
-calculator()
--- ass_ex_two.py
+++ ass_ex_two.py
@@ -9,13 +9,17 @@ def max_of_three(num1, num2, num3):
     except: 
         return print("Invalid inputs. Try again.")
         
-        #this if statement returns the largest number
+        
     else:
+        #this if statement returns the largest number by comparing num1
+        #with both num2 and num3 at the same time
         if num1 > (num2 and num3):
             return print(f"The largest number is: {num1}")
-
+        
+        #the same happens for num2
         elif num2 > (num1 and num3):
             return print(f"The largest number is: {num2}")
         
+        #if neither num1 or num2 are the largest, it automatically means num3 is the largest
         else:
             return print(f"The largest number is: {num3}")
