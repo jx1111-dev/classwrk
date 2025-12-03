dd8465f245056f4fef3a1dbdefff6960f2f258ba
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-03 17:41:33 +0000

    exercise 4 finished

--- ass_ex_four.py
+++ ass_ex_four.py
@@ -1,16 +1,25 @@
-def sum_of_events():
-    min_num = int(input("Enter the minimum number: "))
-    max_num = int(input("Enter the maximum number: "))
+def sum_of_evens():
+    #these two inputs get the maximum and minimum number
 
-    sum = 0
+    try:
+
+        min_num = int(input("Enter the minimum number: "))
+        max_num = int(input("Enter the maximum number: "))
+
+    except:
+
+        return print("Invalid input.")
     
-    for i in range(min_num, max_num):
+    else:
+
+        #the sum will be added to throughout the for statement, so it is declared here
+        sum = 0
         
-
-        if i % 2 == 0:
-            sum += i
+        #the range for the for loop is set from the minimum number to the maximum number
+        for i in range(min_num, max_num):
             
-
-    print(f"Your sum is: {sum}")
-
-sum_of_events()
+            #each iteration, this if statement checks if the number is even. If it is, it is added to the sum.
+            if i % 2 == 0:
+                sum += i
+            
+        print(f"Your sum is: {sum}")
