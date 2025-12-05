56e8c7724c0a9e0af94c1b7c7259f3fb833a22cc
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 14:33:14 +0000

    ex 11 done

--- ass_ex_eleven.py
+++ ass_ex_eleven.py
@@ -1,10 +1,27 @@
 def sum_of_digits(num):
+    #the number is turned into a string, and into a list
+    #so we can iterate through each digit
     num = list(str(num))
+    #x will act as the sum
     x = 0
 
-    for i in num:
-        x+= int(i)
-    
-    return x
+    #input validation
+    try:
 
-print(sum_of_digits(0))
+        for i in num:
+            i = int(i)
+    
+    except:
+
+        return print("Invalid input.")
+    
+    else:
+
+        #iterates through the list (contains all digits),
+        #and adds it to the sum
+        for i in num:
+            x+= int(i)
+        
+    return print(x)
+
+sum_of_digits("24a3")
