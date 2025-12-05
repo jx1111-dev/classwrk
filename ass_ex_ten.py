01e0e7b281386cd411fc52884efb1e2a0bb29643
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 14:21:00 +0000

    ex 10 fixed and done (didnt sort negative numbers

--- ass_ex_ten.py
+++ ass_ex_ten.py
@@ -1,51 +1,25 @@
-num_list = [10, 9, 8, 7]
-
 def sort_list(number_list):
 
     #flag for the while loop
-    flag = True
+    repeat = True
+    #gets the length of the list
+    n = len(number_list)
 
-    while flag:
+    #this while loop only repeats if a number in the list is smaller
+    #than the next one in the list
+    while repeat:
+        repeat = False
 
-        #each iteration of the loop has the index of the character and the character
-        for index, char in enumerate(num_list):
-            next_index = index+1
-            print(index)
-            print(char)
-
-            #compares the current item in the list to the one at the next position.
-            #if it is smaller than the next, it goes to the next iteration.
-            #if the next position is outside of the list, it throws an exception,
-            #ending the loop.
-            try:
-                if char < num_list[next_index]:
-                    next
+        for i in range(n-1):
             
-            #ends the loop if an exception is thrown.
-            except:
-                print(number_list)
-                flag = False
+            #if this if statement fails, it means the list is sorted,
+            #so the while loop doesn't repeat
+            if number_list[i] > number_list[i + 1]:
+                number_list[i], number_list[i + 1] = number_list[i + 1], number_list[i]
+                #if a change does happen, 
+                #repeat is set to true so the program repeats
+                repeat = True
             
-            else:
-                
-                try:
-                    #compares the current character to the one at the next position
-                    if char > num_list[next_index]:
-                            
-                            #if it's bigger, the char is stored as bigger_number
-                            bigger_number = char
-
-                            #the number at the next index is stored as next_number
-                            next_number = num_list[next_index]
-
-                            #the number at the next position is replaced by the bigger number
-                            num_list[next_index] = bigger_number
-
-                            #the number at the current position is replaced by the smaller number
-                            num_list[index] = next_number
-                
-                #in case it fails, it goes to the next loop
-                except:
-                    next     
-
-sort_list(num_list)
+    
+    print(number_list)
+    return number_list
