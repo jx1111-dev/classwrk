0fca94e0f74121d74ca675193d71f5f4a10817bb
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 10:42:56 +0000

    ex 6 done

--- ass_ex_six.py
+++ ass_ex_six.py
@@ -1,14 +1,23 @@
 def calculate_weekly_pay():
+
+    #The normal and overtime salaries are defined
     pay = 12
     overtime = 18
+
+    #input validation
     try:
         hours_done = int(input("Enter the hours you worked: "))
+        
+        if hours_done < 0:
+            return print("Invalid input.")
 
     except:
-        print("Invalid number inputted.")
+        print("Invalid input.")
         return
     
     else:
+        #The program decides if it should apply normal or overtime pay,
+        #then calculates the total pay.
         if hours_done < 35:
             total_pay = hours_done * pay
 
