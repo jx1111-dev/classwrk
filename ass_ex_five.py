3582e38fcf0e5fd9db4669002a09b47236718e84
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 10:36:00 +0000

    ex five finished

--- ass_ex_five.py
+++ ass_ex_five.py
@@ -1,17 +1,26 @@
-num_list = [1, 8, 9]
+def calculate_average(num_list):
 
-def calculate_average():
-    #sum will have each iteration added to it 
-    #list_length will store the length of the list for the end
-    sum = 0
-    list_length = 0
+    try:
 
-    for i in num_list:
-        list_length += 1
-        sum += i
+        for i in num_list:
+            i = float(i)
+
+    except:
+
+        return print("Invalid input. Try again.")
     
-    #floor division is used instead of regular division, to make sure the output is an integer
-    avg = sum // list_length
-    print(f"The average (rounded down) is: {avg}")
+    else:
 
-calculate_average()
+        #sum will have each iteration added to it 
+        #list_length will store the length of the list for calculating the average
+        sum = 0
+        list_length = 0
+
+        #iterates through each item in num_list
+        for i in num_list:
+            list_length += 1
+            sum += i
+        
+        #floor division is used instead of regular division, to make sure the output is an integer
+        avg = sum // list_length
+        print(f"The average (rounded down) is: {avg}")
