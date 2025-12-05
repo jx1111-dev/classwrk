70b098010f97497d99a411a14a9c82ba4c5723ca
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 11:06:09 +0000

    ex 8 done

--- ass_ex_eight.py
+++ ass_ex_eight.py
@@ -1,7 +1,16 @@
 def are_anagrams():
-    word1 = list(input("Enter your first word: ").lower())
-    word2 = list(input("Enter your second word: ").lower())
 
+    #gets the words and stores them as lists
+    #then, all items in the list are made lowercase (to make them the same)
+    #and then they are sorted, which arranges all the letters in the same order,
+    #if the words are the same.
+    word1 = list(input("Enter your first word: ").lower())
+    word1 = word1.sort()
+    word2 = list(input("Enter your second word: ").lower())
+    word2 = word2.sort()
+
+    #compares the elements of each list. If they have the same letters, it means
+    #it's an anagram
     if word1 == word2:
         print("These words are anagrams.")
         return
