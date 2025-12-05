9964971610d742551faf610c58fd3225f105f28b
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 14:52:11 +0000

    ex 14 started

--- ass_ex_fourteen.py
+++ ass_ex_fourteen.py
@@ -1,3 +1,11 @@
-def letter_grade(student_data):
+data_input = [
+{"subject": "Math", "score": 90, "credits": 40},
+{"subject": "History", "score": 75, "credits": 20},
+{"subject": "English", "score": 60, "credits": 20},
+{"subject": "Science", "score": 85, "credits": 40},
+{"subject": "Art", "score": 50, "credits": 40}
+]
+
+def letter_grade(data_input):
     print("a")
 
