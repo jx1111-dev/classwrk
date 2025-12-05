0c4686defce694945f47d476d2825740b7acc8cc
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-05 10:53:05 +0000

    ex 7 done

--- ass_ex_seven.py
+++ ass_ex_seven.py
@@ -1,23 +1,30 @@
-prime_list = [2, 3, 5, 7]
-
 def is_prime():
 
+    #The prime factors are defined here
+    prime_list = [2, 3, 5, 7]
+
+    #input validation
     try:
-        num = int(input("Enter your number: "))
+        num = float(input("Enter your number: "))
     
     except:
         print("Invalid number.")
         return
     
     else:
-        if num > 1:
 
+        #this if statement makes all negative numbers return as false
+        if num > 1:
+            
+            #checks if the number is directly in the prime number list
             if num in prime_list:
                 print("This number is prime.")
                 return
             
             else:
-
+                
+                #if it's not, it determines wether or not it's prime by
+                #checking if the modulus is 0
                 for i in prime_list:
 
                     if num % i == 0:
@@ -32,5 +39,3 @@ def is_prime():
         else:
             print("This number is not prime.")
             return
-
-is_prime()
