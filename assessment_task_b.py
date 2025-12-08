96c085a7d1cb51f780e0c9f54ea8d2819589ab2d
jx1111-dev <buscaalexcatalin@gmail.com>
2025-12-08 16:07:53 +0000

    vending machine pt2

--- assessment_task_b.py
+++ assessment_task_b.py
@@ -1,17 +1,41 @@
+inventory = {
+    1 :{'Name': 'Water','Price': 1.20, 'Stock' : 5}, 
+    2 :{'Name': 'Soda','Price': 1.50, 'Stock' : 5}, 
+    3 :{'Name': 'Chocolate','Price': 2.50, 'Stock' : 5}, 
+    4 :{'Name': 'Crisps','Price': 1.40, 'Stock' : 5}, 
+    5 :{'Name': 'Sandwich','Price': 3.80, 'Stock' : 5}, 
+    }
+
 def vending_machine():
 
-    items = {
-        "Water" :{"price": 1.20, "stock" : 5}, 
-        "Soda" :{"price": 1.50, "stock" : 5}, 
-        "Chocolate" :{"price": 2.50, "stock" : 5}, 
-        "Crisps" :{"price": 1.40, "stock" : 5}, 
-        "sandwich" :{"price": 3.80, "stock" : 5}, 
-        }
+    flag = True
+
+    while flag:
     
-    try:  
-        return
-    except:
-        print()
+        print("---------Welcome to the vending machine---------")
+
+        ans = input("What would you like to do?\n1: Input coins\n2: Purchase an item\n")
+
+        if ans == "1":
+            input_money()
+
+        elif ans == "2":
+            purchase_items()
+        
+        else:
+            input("Invalid input. Press any key to try again.")
+            continue
+
 
 def input_money():
     print()
+
+def purchase_items():
+    for key, value in inventory.items():
+        print(f'{key}: {value['Name']} \tPrice: {value['Price']} \tStock: {value['Stock']}')
+    print()
+
+def print_csv():
+    print()
+
+vending_machine()
